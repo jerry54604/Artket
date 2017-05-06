5e7e73483c8d16775ea3a254d80405b2d6f8492a
jerry54604 <i_54604@hotmail.com>
2017-05-06 21:15:49 +1200

    remove all files

--- src/app/config/firebase.config.ts
+++
@@ -1,7 +0,0 @@
-export const FirebaseConfig = {
-  apiKey: 'AIzaSyC0fpTTHr5B4tuvXijNIiBcUJhcRwEjA4U',
-  authDomain: 'artket-e01a4.firebaseapp.com',
-  databaseURL: 'https://artket-e01a4.firebaseio.com',
-  storageBucket: 'artket-e01a4.appspot.com',
-  messagingSenderId: '499962113939'
-};
--- src/app/interface/user.interface.ts
+++
@@ -1,5 +0,0 @@
-﻿export interface User {
-    email: string;
-    password: string;
-    confirmPassword?: string;
-}
